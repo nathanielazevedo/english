ee7d67122d28b14e5be85e23cec75f0c8980abbe
nathanielazevedo <nathanielazevedo@hotmail.com>
2022-10-04 20:17:40 -0400

    audio added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import "./App.css";
+import nephew from "./audio/nephew.m4a";
+import family from "./audio/family.m4a";
+import grocery from "./audio/grocery.m4a";
+import amazon from "./audio/amazon.m4a";
 
 function App() {
   const words = [
     {
       word: "nephew",
       pronounciation: "ne-few",
-      audio: "../audio/nephew",
+      audio: nephew,
       description: "How is your nephew?",
     },
     {
       word: "family",
-      pronounciation: "fa-mi-lee",
-      audio: "../audio/family",
+      pronounciation: "fam-ehh-lee",
+      audio: family,
       description: "I miss my family. They are in China.",
     },
     {
       word: "grocery",
       pronounciation: "grow-sure-e",
-      audio: "../audio/grocery",
+      audio: grocery,
       description: "I'm going to get groceries.",
     },
     {
       word: "amazon",
       pronounciation: "a-ma-zon",
-      audio: "../audio/grocery",
+      audio: amazon,
       description: "I bought it from amazon.",
     },
   ];
@@ -31,10 +35,13 @@ function App() {
   const Card = (props) => (
     <div className="card">
       <div>
-        {props.word.word}{" "}
+        {props.word.word}
         <span className="pro">| {props.word.pronounciation}</span>
       </div>
       <p className="p">{props.word.description}</p>
+      <audio controls>
+        <source src={props.word.audio} type="audio/mp4" />
+      </audio>
     </div>
   );
 
